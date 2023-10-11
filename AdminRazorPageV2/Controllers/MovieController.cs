1103ed6d6e70e09de5641c35340c4deb2f9cb266
Hungnpv <hungnpv23@gmail.com>
2023-10-11 22:23:06 +0700

    commit

--- AdminRazorPageV2/Controllers/MovieController.cs
+++ AdminRazorPageV2/Controllers/MovieController.cs
@@ -7,163 +7,39 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
 using AdminRazorPageV2.Models;
+using AutoMapper.Execution;
+using System.Text.Json;
+using System.Net.Http.Headers;
 
 namespace AdminRazorPageV2.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class MovieController : Controller
     {
-        private readonly HighFlixContext _context;
+        private readonly HttpClient client = null;
+        private string MovieApiUrl = "";
 
-        public MovieController(HighFlixContext context)
+        public MovieController()
         {
-            _context = context;
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            MovieApiUrl = "http://localhost:7113/api/Movies";
         }
 
         // GET: Movie
         public async Task<IActionResult> Index()
         {
-            var HighFlixContext = _context.Movies.Include(m => m.PostedByUserNavigation);
-            return View(await HighFlixContext.ToListAsync());
-        }
+            HttpResponseMessage response = await client.GetAsync(MovieApiUrl);
+            string strData = await response.Content.ReadAsStringAsync();
 
-        // GET: Movie/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Movies == null)
+            var options = new JsonSerializerOptions
             {
-                return NotFound();
-            }
+                PropertyNameCaseInsensitive = true
+            };
 
-            var movie = await _context.Movies
-                .Include(m => m.PostedByUserNavigation)
-                .FirstOrDefaultAsync(m => m.MovieId == id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
-
-            return View(movie);
-        }
-
-        // GET: Movie/Create
-        public IActionResult Create()
-        {
-            ViewData["PostedByUser"] = new SelectList(_context.Users, "UserId", "UserId");
-            return View();
-        }
-
-        // POST: Movie/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MovieId,PostedByUser,MovieName,MovieThumnailImage,MoviePoster,ListEpisode,TotalEpisodes,Description,ReleasedYear,AliasName,Director,MainCharacters,Trailer,Comments,IsActive")] Movie movie)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(movie);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["PostedByUser"] = new SelectList(_context.Users, "UserId", "UserId", movie.PostedByUser);
-            return View(movie);
-        }
-
-        // GET: Movie/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Movies == null)
-            {
-                return NotFound();
-            }
-
-            var movie = await _context.Movies.FindAsync(id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
-            ViewData["PostedByUser"] = new SelectList(_context.Users, "UserId", "UserId", movie.PostedByUser);
-            return View(movie);
-        }
-
-        // POST: Movie/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("MovieId,PostedByUser,MovieName,MovieThumnailImage,MoviePoster,ListEpisode,TotalEpisodes,Description,ReleasedYear,AliasName,Director,MainCharacters,Trailer,Comments,IsActive")] Movie movie)
-        {
-            if (id != movie.MovieId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(movie);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MovieExists(movie.MovieId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["PostedByUser"] = new SelectList(_context.Users, "UserId", "UserId", movie.PostedByUser);
-            return View(movie);
-        }
-
-        // GET: Movie/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Movies == null)
-            {
-                return NotFound();
-            }
-
-            var movie = await _context.Movies
-                .Include(m => m.PostedByUserNavigation)
-                .FirstOrDefaultAsync(m => m.MovieId == id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
-
-            return View(movie);
-        }
-
-        // POST: Movie/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Movies == null)
-            {
-                return Problem("Entity set 'HighFlixContext.Movies'  is null.");
-            }
-            var movie = await _context.Movies.FindAsync(id);
-            if (movie != null)
-            {
-                _context.Movies.Remove(movie);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool MovieExists(int id)
-        {
-          return (_context.Movies?.Any(e => e.MovieId == id)).GetValueOrDefault();
+            List<Movie> listMovie = JsonSerializer.Deserialize<List<Movie>>(strData, options);
+            return View(listMovie);
         }
     }
 }
