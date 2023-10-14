1516ba63c51cec4b5f14b30439aa26bf875e350c
Dhung <hung57144@gmail.com>
2023-10-14 12:20:01 +0700

    add Delete Template (Delete Category)

--- AdminRazorPageV2/DTOs/CategoryDTOs/ResponseDTOs/CategoryResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DTOs.CategoryDTOs.ResponseDTOs
-{
-    public class CategoryResponse
-    {
-        public int CategoryId { get; set; }
-
-        public string CategoryName { get; set; }
-    }
-}
