e58f1902d6edbb81b9521847ba20f60a52104163
phucnt <nguyentrongphucnn205@gmail.com>
2023-10-29 13:19:19 +0700

    Update change API Url for episode controller

--- AdminRazorPageV2/Controllers/EpisodeController.cs
+++ AdminRazorPageV2/Controllers/EpisodeController.cs
@@ -28,7 +28,7 @@ namespace AdminRazorPageV2.Controllers
             _httpClient = new HttpClient();
             var contentType = new MediaTypeWithQualityHeaderValue("application/json");
             _httpClient.DefaultRequestHeaders.Accept.Add(contentType);
-            MovieManagementApiUrl = "http://localhost:7113/api/Movies";
+            MovieManagementApiUrl = "http://localhost:44384/api/Movies";
             AuthApiUrl = "http://localhost:44388/api/Auth";
             EpisodeManagementApiUrl = "http://localhost:44386/api/Episodes";
             _mapper = mapper;
